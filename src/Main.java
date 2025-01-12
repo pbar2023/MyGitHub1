e515f441eeb5f90ad0bc3e67787e72247c09f934
pbar <pbarabanshikov@yandex.ru>
2025-01-12 19:29:44 +0300

    one more commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, GitHub!");
         System.out.println("Еще привет!!!");
+        System.out.println("Еще привет!!!!");
     }
 }
